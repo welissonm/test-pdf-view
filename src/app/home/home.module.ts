5125bb14c66e7000c49b6de760b048f94af8fa98
Welisson Moura <welissonm@outlook.com>
2022-05-30 18:49:29 -0300

    initial commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+// import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
@@ -12,6 +14,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
+    // PdfViewerModule,
+    NgxExtendedPdfViewerModule,
     HomePageRoutingModule
   ],
   declarations: [HomePage]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import * as pdfjs from 'pdfjs-dist';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  pdfSrc = 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf';
 
-  constructor() {}
+  constructor() {
+    // pdfjs.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+  }
+
+  ngOnInit(): void {
+    // pdfjs.getDocument(this.pdfSrc)
+    // .promise
+    // .then(r => console.log('success', r))
+    // .catch(e => console.log('error', e));
+  }
 
 }
